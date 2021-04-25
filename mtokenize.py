12e63f165ecdfdfd4a3811a2fabc6bcbcce3f52b
Michal Koutný <xm.koutny@gmail.com>
2021-04-25 13:24:18 +0200

    Tokenize numbers generically

--- mtokenize.py
+++ mtokenize.py
@@ -3,14 +3,21 @@
 import nltk
 import pickle
 import sys
+import re
 
 
 if len(sys.argv) > 1:
-    if argv[1] != 'cess_esp':
-        raise ValueError('Unknown corpus {}'.format(argv[1]))
+    if sys.argv[1] != 'cess_esp':
+        raise ValueError('Unknown corpus {}'.format(sys.argv[1]))
     tokens = nltk.corpus.cess_esp.words()
 else:
     tokens = nltk.tokenize.word_tokenize(sys.stdin.read(), language='spanish')
 
+def simplify(w):
+    w = re.sub(r'^\d+$', '<NUM>', w)
+    w = re.sub(r'^[IVXLCDM]+$', '<NUM_R>', w)
+    return w
+
+tokens = list(map(simplify, tokens))
 pickle.dump(tokens, sys.stdout.buffer)
 
