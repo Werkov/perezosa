50caa30a6dae3616a463655dda46ed36ea926bfe
Michal Koutný <xm.koutny@gmail.com>
2021-04-25 13:31:30 +0200

    Ignore stopwords

--- list-ngram.py
+++ list-ngram.py
@@ -2,6 +2,7 @@
 
 import nltk
 import pickle
+import string
 import sys
 
 if len(sys.argv) < 2:
@@ -10,7 +11,13 @@ if len(sys.argv) < 2:
 f = open(sys.argv[1], 'rb')
 finder = pickle.load(f)
 
+ignored_words = nltk.corpus.stopwords.words('spanish')
+ignored_words.extend(c for c in string.punctuation)
+
+finder.apply_word_filter(lambda w: w.lower() in ignored_words)
+
 # TODO support generic ngram
 bigram_measures = nltk.collocations.BigramAssocMeasures()
-best = finder.nbest(bigram_measures.pmi, 20)  
-print(best)
+for b in finder.nbest(bigram_measures.raw_freq, 10000):
+    print(b)
+
--- list-vocabulary.py
+++ list-vocabulary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+import nltk
 import pickle
+import string
 import sys
 
 if len(sys.argv) < 2:
@@ -9,6 +11,12 @@ if len(sys.argv) < 2:
 f = open(sys.argv[1], 'rb')
 counter = pickle.load(f)
 
+ignored_words = nltk.corpus.stopwords.words('spanish')
+ignored_words.extend(c for c in string.punctuation)
+ignored_words.extend(['<num>', '<num_r>'])
+ignnored_wors = set(ignored_words)
 
 for k, v in sorted(counter.items(), key=lambda x: x[1], reverse=True):
+    if k.lower() in ignored_words:
+        continue
     print(k, v)
